20e8b1fd00db1d686abbd4b5428599a4859dab7d
Krischan <krischansteven@gmail.com>
2023-04-24 22:54:32 +0200

    until section 73
    further with section 74

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,9 @@
-'use strict';
+"use strict";
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = document.querySelector(".guess").value;
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number";
+  }
+});
