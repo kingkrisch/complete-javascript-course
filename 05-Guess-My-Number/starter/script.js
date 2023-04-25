adba42f39bf620771abc843b5d40a4c802bd64cd
kingkrisch <kingkrisch@users.noreply.github.com>
2023-04-25 15:23:14 +0200

    shiiiit

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,9 +1,22 @@
 "use strict";
 
-document.querySelector(".check").addEventListener("click", function () {
-  const guess = document.querySelector(".guess").value;
+let secretnumber = Math.trunc(Math.random()*20) +1;
+let highscore = 0;
+let score = 20;
 
-  if (!guess) {
-    document.querySelector(".message").textContent = "No Number";
-  }
-});
+console.log(secretnumber);
+
+document.querySelector(".message").textContent;
+document.querySelector(".score").textContent;
+
+
+if(score === 20){
+// When player wins
+document.querySelector(".number").textContent = secretnumber;
+document.querySelector(".number").style.backgroundColor = '#222';
+document.querySelector(".number").style.border = '5px solid #60b347';
+document.querySelector(".number").style.width = "60rem";
+}
+
+console.log(document.querySelector(".number").textContent);
+console.log(document.querySelector(".guess").value);
--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const btnsOpenModal = document.querySelector('.show-modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+
+for(let i = 0; i < btnsOpenModal.length; i++)
+btnsOpenModal[i].addEventListener('click', function(){
+    console.log('Button Clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+});
+
